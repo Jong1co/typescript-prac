b634a38337726d6d15efdb0d1b6198a78fb7530d
Jongco <narira0922@gmail.com>
2022-10-24 11:19:13 +0900

    typescript calculator

--- 2-types-projects-quiz/calculator.ts
+++ 2-types-projects-quiz/calculator.ts
@@ -1,8 +1,29 @@
 /**
  * Let's make a calculator 🧮
  */
-console.log(calculate('add', 1, 3)); // 4
-console.log(calculate('substract', 3, 1)); // 2
-console.log(calculate('multiply', 4, 2)); // 8
-console.log(calculate('divide', 4, 2)); // 2
-console.log(calculate('remainder', 5, 2)); // 1
+{
+  type Calculator = number | undefined;
+  type Action = "add" | "substract" | "multiply" | "divide" | "remainder";
+  function calculate(func: Action, num1: number, num2: number): Calculator {
+    switch (func) {
+      case "add":
+        return num1 + num2;
+      case "substract":
+        return num1 - num2;
+      case "multiply":
+        return num1 * num2;
+      case "divide":
+        return num1 / num2;
+      case "remainder":
+        return num1 % num2;
+      default:
+        return undefined;
+    }
+  }
+
+  console.log(calculate("add", 1, 3)); // 4
+  console.log(calculate("substract", 3, 1)); // 2
+  console.log(calculate("multiply", 4, 2)); // 8
+  console.log(calculate("divide", 4, 2)); // 2
+  console.log(calculate("remainder", 5, 2)); // 1
+}
