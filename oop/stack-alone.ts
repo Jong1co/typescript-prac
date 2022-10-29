3454a0e9b497d0ec6658b37c7446055c20d082be
Jongco <narira0922@gmail.com>
2022-10-29 11:52:36 +0900

    stack 구현하기
    - 머릿 속으로 어떻게 작동하는지 이해는 갔으나 혼자 만드는데 무리가 있음
    - 구조를 한 번 손으로 그려보는 것도 좋을 거 같음

---
+++ oop/stack-alone.ts
@@ -0,0 +1,40 @@
+interface IStack {
+  readonly size: number;
+  push(value: string): void;
+  pop(): string;
+}
+
+type StackNode = {
+  readonly value: string;
+  readonly next?: StackNode;
+};
+
+class Stack implements IStack {
+  private _size: number = 0;
+  private head?: StackNode;
+  get size(): number {
+    return this._size;
+  }
+  push(value: string): void {
+    const node: StackNode = { value, next: this.head };
+    console.log(node);
+    this.head = node;
+    this._size++;
+  }
+  pop(): string {
+    //이렇게 타입 검사를 해주지 않으면 node.next가 undefined가 나올 수 있기 때문에 미리 타입검사를 해주는 것.
+    if (this.head == null) {
+      throw new Error("Stack is empty!");
+    }
+    const node = this.head;
+    this.head = node.next;
+    this._size--;
+    return node.value;
+  }
+}
+
+const stack = new Stack();
+stack.push("ellie");
+stack.push("peter");
+console.log(stack.pop());
+console.log(stack.pop());
--- oop/stact.ts
+++ oop/stact.ts
@@ -1,48 +1,48 @@
-interface Stack {
-  readonly size: number;
-  push(value: string): void;
-  pop(): string;
-}
+// interface Stack {
+//   readonly size: number;
+//   push(value: string): void;
+//   pop(): string;
+// }
 
-type StackNode = {
-  //불변성 유지를 위한 readonly
-  readonly value: string;
-  readonly next?: StackNode;
-};
+// type StackNode = {
+//   //불변성 유지를 위한 readonly
+//   readonly value: string;
+//   readonly next?: StackNode;
+// };
 
-class StackImpl implements Stack {
-  constructor(private capacity: number) {}
-  private _size: number = 0;
-  private head?: StackNode;
-  get size(): number {
-    return this._size;
-  }
-  push(value: string) {
-    if (this.size === this.capacity) {
-      throw new Error("Stack is full!");
-    }
-    const node: StackNode = { value, next: this.head };
-    console.log(node);
-    this.head = node;
-    this._size++;
-  }
-  pop(): string {
-    if (this.head == null) {
-      throw new Error("Stack is empty!");
-    }
-    const node = this.head;
-    console.log(node);
-    console.log(node.next);
-    this.head = node.next;
-    this._size--;
-    return node.value;
-  }
-}
+// class StackImpl implements Stack {
+//   constructor(private capacity: number) {}
+//   private _size: number = 0;
+//   private head?: StackNode;
+//   get size(): number {
+//     return this._size;
+//   }
+//   push(value: string) {
+//     if (this.size === this.capacity) {
+//       throw new Error("Stack is full!");
+//     }
+//     const node: StackNode = { value, next: this.head };
+//     console.log(node);
+//     this.head = node;
+//     this._size++;
+//   }
+//   pop(): string {
+//     if (this.head == null) {
+//       throw new Error("Stack is empty!");
+//     }
+//     const node = this.head;
+//     console.log(node);
+//     console.log(node.next);
+//     this.head = node.next;
+//     this._size--;
+//     return node.value;
+//   }
+// }
 
-const stack = new StackImpl(10);
-stack.push("ellie");
-stack.push("ellie");
-stack.push("ellie");
-console.log("---");
-stack.pop();
-console.log(stack.size);
+// const stack = new StackImpl(10);
+// stack.push("ellie");
+// stack.push("ellie");
+// stack.push("ellie");
+// console.log("---");
+// stack.pop();
+// console.log(stack.size);
